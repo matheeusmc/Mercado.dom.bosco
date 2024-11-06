1d29824feb9f35faf38d6c5a4ad86558deb16d9c
Matheus lima <matheuslimadasilva0@gmail.com>
2024-11-05 21:09:28 -0300

    aula

---
+++ cad.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+     <h1>Cadastro de clientes | Empório Padaria</h1>
+   </header>
+   
+   <main class="text">
+        <?php
+           $nome = $_GET["nome"];
+           $sobrenome = $_GET["sobrenome"];
+           $telefone = $_GET["telefone"];
+
+               
+           echo "<p>É um prazer te conhecer, a Empório Padaria agradece $nome $sobrenome! Entraremos em contato com você!</p>";
+        ?>
+
+        <button class="text-js">
+        <a href="javascript:history.go(-1)">Voltar para página anterior</a>
+        </button>
+        
+   </main>
+</body>
+</html>
